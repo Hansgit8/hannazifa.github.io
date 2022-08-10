f3cc9e6ff7ad2ddcb7ba2c719ec63aacc0903ede
Hansgit8 <hannanaziifa@gmail.com>
2022-08-10 14:44:54 +0800

    added files

---
+++ main.js
@@ -0,0 +1,10 @@
+
+/*ANIMATIN PORTFOLIO*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+sr.reveal('.portfolio-img', {interval: 200})
